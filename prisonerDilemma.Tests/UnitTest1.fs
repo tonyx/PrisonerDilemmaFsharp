5c664f2574e221791367cf549fa8dd1f43214e8b
Tonyx <tonyx1@gmail.com>
2019-07-09 09:22:19 +0200

    commit

--- prisonerDilemma.Tests/UnitTest1.fs
+++ prisonerDilemma.Tests/UnitTest1.fs
@@ -4,10 +4,9 @@ open NUnit.Framework
 
 open prisonerDilemma.Program
 
-
 [<TestClass>]
-type TestClass () =
 
+type TestClass () =
     [<SetUp>]
     member this.Setup () =
         ()
@@ -18,9 +17,9 @@ type TestClass () =
     
     [<Test>]
     member this.OneTickGameBetweenCooperators() =
-        let cooperatorStrategy = {Name="cooperator";Strategy=cooperatorPlay}
-        let firstCooperator = {Name="cooperator1";StrategyInfo=cooperatorStrategy}
-        let secondCooperator = {Name="cooperator2";StrategyInfo=cooperatorStrategy}
+        let cooperatorStrategyInfo = {Name="cooperator";Strategy=cooperatorStrategy}
+        let firstCooperator = {Name="cooperator1";StrategyInfo=cooperatorStrategyInfo}
+        let secondCooperator = {Name="cooperator2";StrategyInfo=cooperatorStrategyInfo}
         let game = {Player1=firstCooperator;Player2=secondCooperator; JointmoveHistory=[]}
         let oneTickGame = tick game
         let resultingMoves = oneTickGame.JointmoveHistory 
@@ -29,71 +28,61 @@ type TestClass () =
         Assert.That(frameOfGame.Player1Move = Cooperate)
         Assert.That(frameOfGame.Player2Move = Cooperate)
         
-//    [<Test>]
-//    member this.OutcomesTestCooperator() =
-//        let cooperatorStrategy = {Name="cooperator";Strategy=cooperatorPlay}
-//        let firstCooperator = {Name="cooperator1";StrategyInfo=cooperatorStrategy}
-//        let secondCooperator = {Name="cooperator2";StrategyInfo=cooperatorStrategy}
-//        let game = {Player1=firstCooperator;Player2=secondCooperator; JointmoveHistory=[]}
-//        let oneTickGame = tick game
-//        let outcomes = cumulateOutcomes oneTickGame.JointmoveHistory
-//        Assert.That(outcomes.Player1Outcome = 4)
-
 
     [<Test>]
     member this.OutcomesOfATwoTickGameBetweenCooperators() =
-        let cooperatorStrategy = {Name="cooperator";Strategy=cooperatorPlay}
-        let firstCooperator = {Name="cooperator1";StrategyInfo=cooperatorStrategy}
-        let secondCooperator = {Name="cooperator2";StrategyInfo=cooperatorStrategy}
+        let cooperatorStrategyInfo = {Name="cooperator";Strategy=cooperatorStrategy}
+        let firstCooperator = {Name="cooperator1";StrategyInfo=cooperatorStrategyInfo}
+        let secondCooperator = {Name="cooperator2";StrategyInfo=cooperatorStrategyInfo}
         let game = {Player1=firstCooperator;Player2=secondCooperator; JointmoveHistory=[]}
         let twoTicksGame =  game |> tick |> tick
         Assert.That((List.length twoTicksGame.JointmoveHistory)=2)
-        let outcomes = gameOutcomes twoTicksGame 
-        Assert.That(outcomes.Player1Outcome = 8)
+        let outcomes = gameScores twoTicksGame 
+        Assert.That(outcomes.Player1Score = 8)
 
     [<Test>]
     member this.NTicksGameTest() =
-        let cooperatorStrategy = {Name="cooperator";Strategy=cooperatorPlay}
-        let firstCooperator = {Name="cooperator1";StrategyInfo=cooperatorStrategy}
-        let secondCooperator = {Name="cooperator2";StrategyInfo=cooperatorStrategy}
+        let cooperatorStrategyInfo = {Name="cooperator";Strategy=cooperatorStrategy}
+        let firstCooperator = {Name="cooperator1";StrategyInfo=cooperatorStrategyInfo}
+        let secondCooperator = {Name="cooperator2";StrategyInfo=cooperatorStrategyInfo}
         let game = {Player1=firstCooperator;Player2=secondCooperator; JointmoveHistory=[]}
         let twoTicksGame =  nTicks game 2
         Assert.That((List.length twoTicksGame.JointmoveHistory)=2)
-        let outcomes = gameOutcomes twoTicksGame
-        Assert.That(outcomes.Player1Outcome = 8)
+        let outcomes = gameScores twoTicksGame
+        Assert.That(outcomes.Player1Score = 8)
         Assert.That(true)
 
     [<Test>]
     member this.GamesOutcome() =
-        let cooperatorStrategy = {Name="cooperator";Strategy=cooperatorPlay}
-        let firstCooperator = {Name="cooperator1";StrategyInfo=cooperatorStrategy}
-        let secondCooperator = {Name="cooperator2";StrategyInfo=cooperatorStrategy}
+        let cooperatorStrategyInfo = {Name="cooperator";Strategy=cooperatorStrategy}
+        let firstCooperator = {Name="cooperator1";StrategyInfo=cooperatorStrategyInfo}
+        let secondCooperator = {Name="cooperator2";StrategyInfo=cooperatorStrategyInfo}
         let game1 = {Player1=firstCooperator;Player2=secondCooperator; JointmoveHistory=[]}
         let game2 = {Player1=firstCooperator;Player2=secondCooperator; JointmoveHistory=[]}
         let twoTicksGame1 =  nTicks game1 2
         let twoTicksGame2 =  nTicks game2 2
         let games = [twoTicksGame1;twoTicksGame2]
-        let playedGame = gamesOutcomes games
+        let playedGame = gamesScores games
         Assert.That(List.length playedGame = 2)
         let (_,_,outcome) = List.head playedGame
-        Assert.That(outcome.Player1Outcome=8)
+        Assert.That(outcome.Player1Score=8)
 
     
     [<Test>]
     member this.makeTournmentTest1()  =
-        let cooperatorStrategy = {Name="cooperator";Strategy=cooperatorPlay}
-        let firstCooperator = {Name="cooperator1";StrategyInfo=cooperatorStrategy}
-        let secondCooperator = {Name="cooperator2";StrategyInfo=cooperatorStrategy}
+        let cooperatorStrategyInfo = {Name="cooperator";Strategy=cooperatorStrategy}
+        let firstCooperator = {Name="cooperator1";StrategyInfo=cooperatorStrategyInfo}
+        let secondCooperator = {Name="cooperator2";StrategyInfo=cooperatorStrategyInfo}
         let players = [firstCooperator;secondCooperator]
         let tournment = makeTournment players 1
         Assert.That(List.length (tournment.Games) = 2)
 
     [<Test>]
     member this.makeTournmentTest2()  =
-        let cooperatorStrategy = {Name="cooperator";Strategy=cooperatorPlay}
-        let firstCooperator = {Name="cooperator1";StrategyInfo=cooperatorStrategy}
-        let secondCooperator = {Name="cooperator2";StrategyInfo=cooperatorStrategy}
-        let thirdCooperator = {Name="cooperator3";StrategyInfo=cooperatorStrategy}
+        let cooperatorStrategyInfo = {Name="cooperator";Strategy=cooperatorStrategy}
+        let firstCooperator = {Name="cooperator1";StrategyInfo=cooperatorStrategyInfo}
+        let secondCooperator = {Name="cooperator2";StrategyInfo=cooperatorStrategyInfo}
+        let thirdCooperator = {Name="cooperator3";StrategyInfo=cooperatorStrategyInfo}
         let players = [firstCooperator;secondCooperator;thirdCooperator]
         let tournment = makeTournment players 1
         Assert.That(List.length (tournment.Games) = 6)
@@ -101,17 +90,41 @@ type TestClass () =
 
     [<Test>]
     member this.playTournmentTest()  =
-        let cooperatorStrategy = {Name="cooperator";Strategy=cooperatorPlay}
-        let firstCooperator = {Name="cooperator1";StrategyInfo=cooperatorStrategy}
-        let secondCooperator = {Name="cooperator2";StrategyInfo=cooperatorStrategy}
-        let thirdCooperator = {Name="cooperator3";StrategyInfo=cooperatorStrategy}
+        let cooperatorStrategyInfo = {Name="cooperator";Strategy=cooperatorStrategy}
+        let firstCooperator = {Name="cooperator1";StrategyInfo=cooperatorStrategyInfo}
+        let secondCooperator = {Name="cooperator2";StrategyInfo=cooperatorStrategyInfo}
+        let thirdCooperator = {Name="cooperator3";StrategyInfo=cooperatorStrategyInfo}
         let players = [firstCooperator;secondCooperator;thirdCooperator]
         let tournment = makeTournment players 2
         let playedTournment = playTournment tournment
         let firstGame = List.head playedTournment
         Assert.That(List.length firstGame.JointmoveHistory = 2)
 
-
-        
-
+    [<Test>]
+    member this.tournmentScores()  =
+        let cooperatorStrategyInfo = {Name="cooperator";Strategy=cooperatorStrategy}
+        let firstCooperator = {Name="cooperator1";StrategyInfo=cooperatorStrategyInfo}
+        let secondCooperator = {Name="cooperator2";StrategyInfo=cooperatorStrategyInfo}
+        let players = [firstCooperator;secondCooperator]
+        let tournment = makeTournment players 2
+        let playedTournment = playTournment tournment
+        let outcomes = gamesScores playedTournment
+        let (_,_,firstGameOutcome) = List.head outcomes
+        Assert.That(firstGameOutcome.Player1Score = 8)
+    
+    [<Test>]
+    member this.playersScoreInTournment()  =
+        let cooperatorStrategyInfo = {Name="cooperator";Strategy=cooperatorStrategy}
+        let firstCooperator = {Name="cooperator1";StrategyInfo=cooperatorStrategyInfo}
+        let secondCooperator = {Name="cooperator2";StrategyInfo=cooperatorStrategyInfo}
+        let players = [firstCooperator;secondCooperator]
+        let tournment = makeTournment players 2
+        let playedTournment = playTournment tournment
+        let outcomes = gamesScores playedTournment
+        let (player1,_,firstGameOutcome) = List.head outcomes
+        let (player2,_,secondGameOutcome) = List.head (List.tail outcomes)
+        let player1outcomes = scoreForPlayer playedTournment "cooperator1"
+        Assert.That(firstGameOutcome.Player1Score = 8)
+        Assert.That(secondGameOutcome.Player1Score = 8)
+        Assert.That(16 = player1outcomes)
 
--- prisonerDilemma/Library.fs
+++ prisonerDilemma/Library.fs
@@ -3,14 +3,11 @@
 module Program =
 
     open System
-
     type Move = Cooperate | Defect
-
     type JointMove = {Player1Move: Move; Player2Move: Move}
+    type JointScore = {Player1Score: int; Player2Score: int}
 
-    type JointOutcome = {Player1Outcome: int; Player2Outcome: int}
-
-    let jointOutcome jointMove  =
+    let jointScore jointMove  =
         let R = 4 // my outcome if I cooperate and the other cooperates
         let S = 2 // my outcome if I cooperate and the other defects
         let T = 5 // my outcome if I defect ant the other cooperates
@@ -19,26 +16,21 @@ module Program =
         let _ = if ( not checkPDConditions) then failwith "this is not a pd game: violated  pd conditions: T > R > P > S"
          
         match jointMove with
-            | {Player1Move= Cooperate ; Player2Move=Cooperate} -> {Player1Outcome=R; Player2Outcome=R }
-            | {Player1Move= Cooperate ; Player2Move=Defect} ->    {Player1Outcome=S; Player2Outcome=T }
-            | {Player1Move= Defect ; Player2Move=Cooperate} ->    {Player1Outcome=T; Player2Outcome=S} 
-            | {Player1Move= Defect ; Player2Move=Defect} ->       {Player1Outcome=P; Player2Outcome=P}   
+            | {Player1Move= Cooperate ; Player2Move=Cooperate} -> {Player1Score=R; Player2Score=R }
+            | {Player1Move= Cooperate ; Player2Move=Defect} ->    {Player1Score=S; Player2Score=T }
+            | {Player1Move= Defect ; Player2Move=Cooperate} ->    {Player1Score=T; Player2Score=S} 
+            | {Player1Move= Defect ; Player2Move=Defect} ->       {Player1Score=P; Player2Score=P}   
 
     
     type Strategy = JointMove list -> Move
-
     type StrategyInfo = {Name: string; Strategy:Strategy}
-
     type Player = {Name: string; StrategyInfo: StrategyInfo}
-
     type Game = { Player1:Player; Player2:Player; JointmoveHistory: JointMove list }
-
         
     let tick (game:Game) = 
         let playerOneMove = game.Player1.StrategyInfo.Strategy game.JointmoveHistory 
         let playerTwoMove = game.Player2.StrategyInfo.Strategy game.JointmoveHistory 
         {game with JointmoveHistory = {Player1Move=playerOneMove;Player2Move=playerTwoMove}::game.JointmoveHistory } 
-       // {Player1Outcome=0;Player2Outcome=0} 
         
     let nTicks (game:Game) n =
         [1 .. n ] |> List.fold (fun acc _ -> tick acc ) game
@@ -49,53 +41,59 @@ module Program =
 
     let makeTournment players iterationsPerGame = 
         let games = (players |> List.map (fun x -> ((players |> List.filter (fun z -> z.Name <> x.Name)) |> 
-        (List.map (fun y ->  {Player1=x;Player2=y;JointmoveHistory=[]}  ))))) |> List.fold (@) []
+        (List.map (fun y ->  {Player1=x;Player2=y;JointmoveHistory=[]}  ))))) |> List.fold (@) [] 
         {Games = games;IterationsPerGame=iterationsPerGame}
 
     let playTournment (tournment:Tournment) = 
         tournment.Games |> List.map (fun x -> nTicks x tournment.IterationsPerGame)
 
-
-    let gameOutcomes game =  
+    let gameScores game =  
         game.JointmoveHistory |> List.fold (fun acc x -> 
-            let jo = jointOutcome x;
-            {Player1Outcome=jo.Player1Outcome+acc.Player1Outcome;
-             Player2Outcome=jo.Player2Outcome+acc.Player2Outcome
+            let jo = jointScore x;
+            {Player1Score=jo.Player1Score+acc.Player1Score;
+             Player2Score=jo.Player2Score+acc.Player2Score
             })
-            {Player1Outcome=0;Player2Outcome=0} 
+            {Player1Score=0;Player2Score=0} 
 
-    let gamesOutcomes games =
-        games |> List.map (fun (x:Game) -> (x.Player1,x.Player2,gameOutcomes x))
+    let gamesScores games =
+        games |> List.map (fun (x:Game) -> (x.Player1,x.Player2,gameScores x))
 
-    let (randomPlay:Strategy) = 
+    let scoreForPlayer games playerName =
+        let ot = gamesScores games
+        let gamesPlayer1First =  ot |> List.filter (fun (player1,_,_) -> player1.Name = playerName)
+        let gamesPlayer2First =  ot |> List.filter (fun (_,player2,_) -> player2.Name = playerName)
+        let scoresOfPlayerAsFirst =  gamesPlayer1First |> List.sumBy (fun (_,_,x) -> x.Player1Score)
+        let scoresOfPlayerAsSecond = gamesPlayer2First |> List.sumBy (fun (_,_,x) -> x.Player2Score)
+        scoresOfPlayerAsFirst+scoresOfPlayerAsSecond
+
+    let (randomStrategy:Strategy) = 
         fun (_:JointMove list) ->
             match rand.Next(0,2) with
                 | 0 -> Defect
                 | _ -> Cooperate
 
-    let (defectorPlay:Strategy) =
+    let (defectorStrategy:Strategy) =
         fun _ ->
             Defect
 
-    let (cooperatorPlay:Strategy) =
+    let (cooperatorStrategy:Strategy) =
         fun _ ->
             Cooperate
 
-
-    let (titForTat:Strategy) =
+    let (titForTatStrategy:Strategy) =
         fun (history: JointMove list) ->
             match history with
             | [] -> Cooperate
             | H::_ -> H.Player2Move
 
+    let cooperatorStrategyInfo = {Name="cooperatorStrategy";Strategy=cooperatorStrategy}
+    let defectorStrategyInfo = {Name="defectorStrategy"; Strategy=defectorStrategy}
+    let randomStrategyInfo = {Name="randomStrategy";Strategy=randomStrategy}
+    let titForTatStrategyInfo = {Name="titForTatStrategy";Strategy=titForTatStrategy}
 
+    let cooperatorPlayer = {Name="cooperator";StrategyInfo=cooperatorStrategyInfo}
+    let defectorPlayer= {Name="defector";StrategyInfo=defectorStrategyInfo}
+    let randomPlayer= {Name="random";StrategyInfo=randomStrategyInfo}
+    let titForTatPlayer= {Name="titForTat";StrategyInfo=titForTatStrategyInfo}
 
-
-
-
-
-
-    let hello name =
-        printfn "Hello %s" name
-    let prisoner x = raise(System.NotImplementedException("not implemented"))
-            
+          
